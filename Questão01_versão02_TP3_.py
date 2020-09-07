77446edddbad9eec42b5c76f7754d5672571635c
marianabsctba <62853909+marianabsctba@users.noreply.github.com>
2020-09-07 20:29:34 -0300

    Update Questão01_versão02_TP3_.py

--- Questão01_versão02_TP3_.py
+++ Questão01_versão02_TP3_.py
@@ -1,43 +1,56 @@
 #Essa é a versão 02 da Questão 01, com if/elif/else aninhados... não é a minha preferência, mas fiz um exemplo.
 
-print("\033[1;96m~=\033[0;0m" * 30)
+def lin():
+    print("\033[1;96m~=\033[0;0m" * 30)
+
+def lin_2():
+    print("\033[1;96m--\033[0;0m" * 30)
+
+def lin_3():
+    print("\033[1;96m==\033[0;0m" * 30)
+
+lin()
+
 print("""\033[1;34mOLÁ, BEM-VINDO(a) AO SEUBOLSO,
 A SUA MELHOR OPÇÃO PARA DIVIDIR CONTAS DE RESTAURANTE!\033[0;0m""")
-print("\033[1;96m~=\033[0;0m" * 30)
+
+lin()
+
 
 
 def calc():
     while True:
         consumo = float(input("Digite o valor do consumo: R$ "))
         if consumo < 0:
-            print("\033[1;96m--\033[0;0m" * 30)
+            lin_2()
             print("\033[1;91mOoops...Valor inválido. Programa interrompido.\033[0;0m")
-            print("\033[1;96m--\033[0;0m" * 30)
+            lin_2()
             exit()
         else:
             numero_pessoas = int(input("Digite o n° de pessoas que dividirão a conta: "))
             if numero_pessoas < 0:
-                print("\033[1;96m--\033[0;0m" * 30)
+                lin_2()
                 print("\033[1;91mOoops...Valor inválido. Programa interrompido.\033[0;0m")
-                print("\033[1;96m--\033[0;0m" * 30)
+                lin_2()
                 exit()
             else:
                 percentual_servico = int(input("Digite o percentual do serviço (entre 0 a 100): % "))
                 if percentual_servico < 0 or percentual_servico > 100:
                     print("\033[1;91mOoops...Valor inválido. Programa interrompido.\033[0;0m")
+                    lin_2()
                     exit()
                 else:
                     total_conta = consumo + consumo * (percentual_servico / 100)
                     valor_pessoa = total_conta / numero_pessoas
-                    print("\033[1;96m==\033[0;0m" * 30)
+                    lin_3()
                     print(f"\033[1;34mO valor total da conta é de R$ {total_conta:.2f}\033[0;0m".replace(".", ","))
-                    print("\033[1;96m==\033[0;0m" * 30)
+                    lin_3()
                     print(f"\033[1;34mO total da conta dividido por {numero_pessoas} pessoas é de R$ {valor_pessoa:.2f}\033[0;0m".replace(".", ","))
+                    break
 
-                    print("\033[1;96m~=\033[0;0m" * 30)
-                    print("\033[1;34mOBRIGADO POR USAR SEUBOLSO. VOLTE SEMPRE!\033[0;0m")
-                    print("\033[1;96m~=\033[0;0m" * 30)
-                    exit()
+
 calc()
 
-
+lin()
+print("\033[1;34mOBRIGADO POR USAR SEUBOLSO. VOLTE SEMPRE!\033[0;0m")
+lin()
