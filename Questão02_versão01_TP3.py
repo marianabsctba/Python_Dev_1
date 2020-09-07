63bfaf1140de79b71898039e8c40abeee463f0cc
marianabsctba <62853909+marianabsctba@users.noreply.github.com>
2020-09-07 20:37:46 -0300

    Update Questão02_versão01_TP3.py

--- Questão02_versão01_TP3.py
+++ Questão02_versão01_TP3.py
@@ -1,18 +1,22 @@
 # Essa é a versão 01 da questão 02, que usa a idade em inteiros para fazer a verificação do eleitor.
 # se a idade for inválida (<0) o programa pedirá uma válida.
 
-import time
+def lin():
+    print("\033[1;33m==\033[0;0m" * 30)
 
-print("\033[1;32m==\033[0;0m" * 30)
+lin()
+
 print("Olá, esse é o simulador para eleitores \033[1;32mDemocratic\033[1;33mChoice.\033[0;0m")
 print("""
 \033[1;34mInstruções:\033[0;0m
 
-1° - Digite a sua idade em inteiros, no formato "12", "14" (exemplos).
-2° - Datas de nascimento futuras não serão computadas.
+
+1° - Digite a sua idade em inteiros, no formato "NN", "14", "85" (exemplos).
+2° - Idades inferiores a zero não serão computadas.
 
 \033[1;34mVamos lá? Verique se você é eleitor obrigatório ou não:\033[0;0m """)
-print("\033[1;33m==\033[0;0m" * 30)
+
+lin()
 
 
 
@@ -28,10 +32,13 @@ def voto(idade):
             return f"Com a idade de {idade} anos, o voto é obrigatório."
 
 
+
 eleitor = int(input("Qual a sua idade? "))
 
 print(voto(eleitor))
 
-print("\033[1;33m==\033[0;0m" * 30)
+lin()
+
 print("Obrigado por usar \033[1;32mDemocratic\033[1;33mChoice\033[0;0m. Volte sempre!")
-print("\033[1;33m==\033[0;0m" * 30)
+
+lin()
