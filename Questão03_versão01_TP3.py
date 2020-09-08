3f993f35b4ef3a4cf11db7ddbd43de5e5b7ec6ca
marianabsctba <62853909+marianabsctba@users.noreply.github.com>
2020-09-07 21:01:01 -0300

    Update Questão03_versão01_TP3.py

--- Questão03_versão01_TP3.py
+++ Questão03_versão01_TP3.py
@@ -1,9 +1,14 @@
 #Essa é a versão 01 da Questão 03. Verifico com listas e imprimo os dados com elas.
-#Essa versão verifica e imprime na tela se a nota é >= 0 ou >=10.
+#Essa versão verifica e imprime na tela se a nota é >= 0 ou >=10 e retorna o input caso os valores sejam <0 ou > 10.
 
-print("\033[1;34m=~\033[1;92m=~\033[0;0m" * 20)
+def lin():
+    print("\033[1;34m==\033[1;92m==\033[0;0m" * 20)
+
+lin()
+
 print("\033[1;34mBEM-VINDO AO JURYMANFANTASY, O MELHOR PROGRAMA PARA JURADOS!\033[0;0m")
-print("\033[1;34m=~\033[1;92m=~\033[0;0m" * 20)
+
+lin()
 
 
 def vencedor_concurso():
@@ -23,7 +28,8 @@ def vencedor_concurso():
                 print("\033[32m" + "A nota é igual a zero." + "\033[0;0m")
             else:
                 nota = float(input("\033[31m" + "Oops..." + "Digite uma nota válida (0 a 10): " + "\033[0;0m"))
-            print("\033[1;34m==\033[1;92m==\033[0;0m" * 20)
+
+            lin()
 
             temp.append(nota)
             if len(princ) == 0:
@@ -37,20 +43,22 @@ def vencedor_concurso():
             temp.clear()
 
         print("\033[34m" + f"Você cadastrou {len(princ)} pessoas." + "\033[0;0m")
-        print("\033[33m" + f"A maior nota é {mai}, do(a) VENCEDOR", end=" ")
+        print("\033[33m" + f"A maior nota é {mai}, do(s) VENCEDOR(ES)", end=" ")
         for n in princ:
             if n[1] == mai:
-                print(f"[{n[0]}]." + "\033[0;0m", end=" ")
-        print("\033[31m" + f"A menor nota é {men}, do(a) participante", end=" ")
+                print(f"[{n[0]}]" + "\033[0;0m", end=" ")
+        print("\033[31m" + f". A menor nota é {men}, do(a) participante", end=" ")
         for n in princ:
             if n[1] == men:
-                print(f"[{n[0]}]." + "\033[0;0m", end=" ",)
-                exit()
-        print(vencedor_concurso())
+                print(f"[{n[0]}]." + "\033[0;0m")
 
+                lin()
+
+                print("Obrigado pela preferência! Volte sempre!")
+
+                lin()
+                exit()
 
 vencedor_concurso()
+print(vencedor_concurso())
 
-print("\033[1;34m=~\033[1;92m=~\033[0;0m" * 20)
-print("Obrigada pela preferência! Volte sempre!")
-print("\033[1;34m=~\033[1;92m=~\033[0;0m" * 20)
